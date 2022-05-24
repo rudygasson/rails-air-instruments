7f1544387389d422070a5052dcbcef5ff4d8862a
Christian Rudolf Hoppe <2715405+rudygasson@users.noreply.github.com>
2022-05-24 17:44:04 +0200

    Implement case insensitve match search, conditional view of results

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -2,6 +2,6 @@ class InstrumentsController < ApplicationController
   def index
     @instruments = Instrument.all
     @search_term = params[:search][:instrument_type]
-    @filtered_instruments = @instruments.where(instrument_type: @search_term)
+    @filtered_instruments = @instruments.where('lower(instrument_type) like ?', "%#{@search_term.downcase}%")
   end
 end
