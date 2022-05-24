2707c8ee41012f6f11cfc6c1b7d151e593a02d25
Philipp Riedel <riedel.philipp@gmx.net>
2022-05-24 11:49:04 +0200

    add instrument validations

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,8 @@
 class Instrument < ApplicationRecord
   belongs_to :user
+
+  validates :type, presence: true
+  validates :model, presence: true
+  validates :price_per_day, presence: true
+  validates :location, presence: true
 end
