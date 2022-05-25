0f3f0d1e5bbbbea603592178f46b8acfbf4f1ba2
Christian Rudolf Hoppe <2715405+rudygasson@users.noreply.github.com>
2022-05-25 12:49:49 +0200

    Add Instrument feature for logged-in users

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,8 +1,14 @@
 class InstrumentsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+
   def index
     @instruments = Instrument.all
-    @search_term = params[:search][:instrument_type]
-    @filtered_instruments = @instruments.where('lower(instrument_type) like ?', "%#{@search_term.downcase}%")
+    if params[:search]
+      @search_term = params[:search][:instrument_type]
+      @filtered_instruments = @instruments.where('lower(instrument_type) like ?', "%#{@search_term.downcase}%")
+    else
+      @filtered_instruments = @instruments
+    end
   end
 
   def show
@@ -10,12 +16,32 @@ class InstrumentsController < ApplicationController
   end
 
   def new
+    @user = current_user
+    @instrument = Instrument.new
   end
 
   def create
+    @user = current_user
+    @instrument = Instrument.new(instrument_params)
+    @instrument.user = @user
+    if @instrument.save
+      redirect_to instrument_path(@instrument)
+    else
+      render(:new, status: :unprocessable_entity)
+    end
   end
 
   # private
+
+  def instrument_params
+    params.require(:instrument).permit(
+      :instrument_type,
+      :model,
+      :description,
+      :location,
+      :price_per_day
+    )
+  end
 
   # def set_instrument
   #   @instrument = Instrument.find(params[:id])
